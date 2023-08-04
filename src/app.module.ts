643883a9b10e825f9356828667e1b5f029489c42
Hassan Rizwan Bhutta <96540237+HassanBhutt@users.noreply.github.com>
2023-08-04 20:29:21 +0500

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,7 @@ import { User } from './user/entities/user.entity';
       host: 'localhost',
       port: 3306,
       username: 'root',
-      password: 'mehran@123456789',
+      password: '',
       database: 'hassandb',
       entities: [User],
       synchronize: true,
